0a03255be952b86448ba1c5c2c3919ceb5ed9383
waqhasahmed97 <120351119+waqhasahmed97@users.noreply.github.com>
2024-09-07 12:12:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors'); // Import cors
 const app = express();
-const port = 3001;
+const port = 3000;
 
 // MySQL connection configuration
 const dbConfig = {
